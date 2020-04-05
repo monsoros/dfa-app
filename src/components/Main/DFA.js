22e4ed66869247a4b18a71c753362ce2efe63771
unknown <l.jiraphat@gmail.com>
2020-04-06 01:15:42 +0700

    update

--- src/components/Main/DFA.js
+++ src/components/Main/DFA.js
@@ -5,9 +5,10 @@ import State from './State'
 
 function DFA() {
     let [shareState, setShareState] = useState([])
+    let [password, setpassword] = useState(["0","1","2","0"])
 
     const updateChange = (shareValue) => {
-        setShareState(shareValue)
+        
         if(shareValue[shareValue.length -1]===password[0]){
             check[0]="green"
         }
@@ -25,9 +26,24 @@ function DFA() {
                 check[2]="green"
             }
         }
+        if(shareValue.length>3){
+            if(shareValue[shareValue.length -4]===password[0]&&shareValue[shareValue.length-3]===password[1]&&shareValue[shareValue.length-2]===password[2]){
+                check[0]="green"
+                check[1]="green"
+                check[2]="green"
+                if(shareValue[shareValue.length -1]==="1"||shareValue[shareValue.length -1]==="2"||shareValue[shareValue.length -1]==="3"){
+                    setpassword([password[0],password[1],password[2],"0"])
+                }
+                if(shareValue[shareValue.length -1]==="0"){
+                    setpassword([password[0],password[1],password[2],"1"])
+                    shareValue[shareValue.length -1]="9"
+                    shareValue[shareValue.length -2]="9"
+                }
+            }
+        }
+        setShareState(shareValue)
         
     }
-    var password=["0","1","2"]
     var check=["white","white","white"]
     
 
@@ -51,8 +67,4 @@ function DFA() {
         </div>
     )
 }
-//<link rel="stylesheet" href="/css/video-react.css" />
-/*<Player
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-    />*/
 export default DFA;
