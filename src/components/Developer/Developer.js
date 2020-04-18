75c118b0ce314785e76303e0731253cb0ca42611
Mon Soros <soros.spr@gmail.com>
2020-04-18 19:36:26 +0700

    Update UI 2.0

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -3,8 +3,7 @@ import React from 'react';
 function Developer() {
     return(
         <div className="devContainer">
-            <h1>Developers</h1>
-            <h3>Our team is made of experienced and first-time developers who will work when deadline is coming.</h3>
+            <h1><b>Developers</b></h1>
             <div className="liDevContainer">
                 <div className="devProf">
                     <img src="https://i1.wp.com/www.eoric.uenr.edu.gh/wp-content/uploads/2019/08/male-profile-blank.jpg?ssl=1" alt="" className="devPic"></img>
