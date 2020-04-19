22add7c80507d6cddfed3a6106bdc5d585849b49
Mon Soros <soros.spr@gmail.com>
2020-04-19 14:11:47 +0700

    Add devs photo

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -6,35 +6,35 @@ function Developer() {
             <h1><b>Developers</b></h1>
             <div className="liDevContainer">
                 <div className="devProf">
-                    <img src="https://i1.wp.com/www.eoric.uenr.edu.gh/wp-content/uploads/2019/08/male-profile-blank.jpg?ssl=1" alt="" className="devPic"></img>
+                    <img src={require("../../public/devs/gene.jpg")} alt="gene.jpg" className="devPic"></img>
                     <div className="devName">
                         <p>Jiraphat Lertpipitkul</p>
                         <p>60010140</p>
                     </div>
                 </div>
                 <div className="devProf">
-                    <img src="https://i1.wp.com/www.eoric.uenr.edu.gh/wp-content/uploads/2019/08/male-profile-blank.jpg?ssl=1" alt="" className="devPic"></img>
+                    <img src={require("../../public/devs/tua.png")} alt="tua.png" className="devPic"></img>
                     <div className="devName">
                         <p>Jirayu Aowcharoen</p>
                         <p>60010154</p>
                     </div>
                 </div>
                 <div className="devProf">
-                    <img src="https://i1.wp.com/www.eoric.uenr.edu.gh/wp-content/uploads/2019/08/male-profile-blank.jpg?ssl=1" alt="" className="devPic"></img>
+                    <img src={require("../../public/devs/tontey.jpeg")} alt="tontey.jpg" className="devPic"></img>
                     <div className="devName">
                         <p>Pitawat Chaivutinun</p>
                         <p>60010714</p>
                     </div>
                 </div>
                 <div className="devProf">
-                    <img src="https://i1.wp.com/www.eoric.uenr.edu.gh/wp-content/uploads/2019/08/male-profile-blank.jpg?ssl=1" alt="" className="devPic"></img>
+                    <img src={require("../../public/devs/pose.jpg")} alt="pose.jpg" className="devPic"></img>
                     <div className="devName">
                         <p>Sedsakol Kamolchote</p>
                         <p>60011008</p>
                     </div>
                 </div>
                 <div className="devProf">
-                    <img src="https://i1.wp.com/www.eoric.uenr.edu.gh/wp-content/uploads/2019/08/male-profile-blank.jpg?ssl=1" alt="" className="devPic"></img>
+                    <img src={require("../../public/devs/mon.jpg")} alt="mon.jpg" className="devPic"></img>
                     <div className="devName">
                         <p>Soros Seingpairod</p>
                         <p>60011121</p>
