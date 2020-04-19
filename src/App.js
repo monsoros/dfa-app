844a35eecb44d9d088afa4f63100a85078c3a884
Mon Soros <soros.spr@gmail.com>
2020-04-19 15:38:24 +0700

    Fix router v2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Nav from './components/Main/Nav';
 import Developer from './components/Developer/Developer';
 import DFA from './components/Main/DFA';
@@ -10,22 +10,20 @@ import ReactNotifications from 'react-notifications-component';
 
 function App() {
   return (
-    <HashRouter basename='/'>
-      <div className="App">
-        <header>
-          <Nav />
-        </header>
-        <div className="main">
-          <ReactNotifications />
-          <Route exact path="/" component={DFA}></Route>
-          <Route path="/docs" component={Docs}></Route>
-          <Route path="/developers" component={Developer}></Route>
-        </div>
-        <footer>
-          <Footer />
-        </footer>
+    <div className="App">
+      <header>
+        <Nav />
+      </header>
+      <div className="main">
+        <ReactNotifications />
+        <Route exact path="/" component={DFA}></Route>
+        <Route path="/docs" component={Docs}></Route>
+        <Route path="/developers" component={Developer}></Route>
       </div>
-    </HashRouter>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter basename="/">
         <App />
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
